9da394ec92b40a5936146fb4c950fe1f1b7b57f9
LucioFurnari <luciofurnari99@gmail.com>
2022-05-01 20:24:19 -0300

    probando setTimeout

--- Practica_Nucba/scriptTres.js
+++ Practica_Nucba/scriptTres.js
@@ -7,3 +7,35 @@ function ejecutarApi(e) {
     console.log(e);
     console.log(form);
 }
+
+const title = document.querySelector(".title");
+const text = document.querySelector(".text");
+
+function changeColor() {
+    title.style.color = "red";
+    console.log(title);
+}
+
+function changeText() {
+    text.textContent = " cambio del texto";
+    console.log(text);
+}
+
+function createDiv() {
+    for(let i=0;i < 5;i++){
+        div = document.createElement("div");
+        div.classList.add("divClass")
+        document.body.appendChild(div);
+        
+    }
+    div.addEventListener("click", ()=>{
+        window.setTimeout(()=>{document.querySelector(`.divClass`).style.backgroundColor = "green"}, 2000)
+    })
+}
+
+
+
+window.setTimeout(changeColor, 2000);
+window.setTimeout(changeText, 2000);
+window.setTimeout(createDiv, 2000)
+
