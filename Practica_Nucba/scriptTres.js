fe034ebddfce020206a804968b83f265ed24fa79
LucioFurnari <luciofurnari99@gmail.com>
2022-05-02 21:21:21 -0300

    -

--- Practica_Nucba/scriptTres.js
+++ Practica_Nucba/scriptTres.js
@@ -1,3 +1,43 @@
+//Primer parrafo
+const parrafo1 = document.createElement("p");
+parrafo1.textContent = "El parrafo xd";
+parrafo1.classList.add("categoria", "paseo");
+
+
+//Segundo parrafo
+const parrafo2 = document.createElement("p");
+parrafo2.textContent="Titulo parrafo";
+parrafo2.classList.add("titulo");
+
+//Tercer parrafo
+const parrafo3 = document.createElement("p");
+parrafo3.textContent = "$2500 mensuales";
+parrafo3.classList.add("precio");
+
+//Div
+
+const info = document.createElement("div");
+info.classList.add("info");
+info.appendChild(parrafo1);
+info.appendChild(parrafo2);
+info.appendChild(parrafo3);
+document.body.appendChild(info);
+
+//Crear Img
+const img = document.createElement("img");
+img.src = "imagen.jpg";
+
+
+//Creamos el div contenedor CARD
+const contenedorCard = document.createElement("div");
+contenedorCard.classList.add("contenedor-cards");
+contenedorCard.appendChild(img);
+contenedorCard.appendChild(info);
+document.body.appendChild(contenedorCard);
+
+
+
+//////////////////
 const form = document.querySelector("#formulario");
 console.log(form);
 form.addEventListener("submit", ejecutarApi);
