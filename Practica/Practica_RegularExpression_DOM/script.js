a532e7a88937a778bba51cb2462e75df99c1e9d7
LucioFurnari <luciofurnari99@gmail.com>
2022-05-07 20:13:06 -0300

    Cambio en el codigo, para optimizarlo

--- Practica/Practica_RegularExpression_DOM/script.js
+++ Practica/Practica_RegularExpression_DOM/script.js
@@ -1,24 +1,37 @@
+//Variables
 const form = document.getElementsByClassName("formulario");
-const inputs = document.querySelector(".formulario,input");
-const alert = document.querySelector(".alert");
+const inputs = document.querySelector("#formulario input");
+// const alert = document.querySelector(".alert");
 
+//Expresiones/Regex
 const expresiones = {
     usuario: /^[a-zA-Z]{0,32}$/,
     password: /^[a-zA-Z0-9]{0,32}$/
 };
+
+//Funciones
+const alertCorrect = (param) => {
+    param.target.classList.add("formulario_correcto");
+    param.target.classList.remove("formulario_incorrecto");
+}
+const alertIncorrect = (param) =>{
+    param.target.classList.remove("formulario_correcto");
+    param.target.classList.add("formulario_incorrecto");
+}
 
 const validarFormulario = (e) => {
     switch (e.target.name) {
         case "usuario":
             if(e.target.value.match(expresiones.usuario)){
                 console.log("Correcto");
-                alert.classList.add("alert");
-                alert.classList.remove("formulario_incorrecto");
-                
+                alertCorrect(e);
+                // inputs.classList.add("formulario_correcto");
+                // inputs.classList.remove("formulario_incorrecto");               
             }else{
                 console.log("Incorrecto");
-                alert.classList.remove("alert");
-                alert.classList.add("formulario_incorrecto");
+                alertIncorrect(e);
+                // inputs.classList.remove("formulario_correcto");
+                // inputs.classList.add("formulario_incorrecto");
             }
             break;
         case "password":
