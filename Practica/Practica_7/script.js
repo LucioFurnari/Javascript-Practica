d7b7cc0e086ecf7034919bbd98bace09d23b3015
LucioFurnari <luciofurnari99@gmail.com>
2022-05-14 16:42:22 -0300

    -

--- Practica/Practica_7/script.js
+++ Practica/Practica_7/script.js
@@ -1,16 +1,29 @@
-//Selectores
+// Selectores
 const $Selector = document.querySelector("#selector");
+const $SelectorTwo = document.querySelector("#selectorTwo");
+const $SelectorThree = document.querySelector("#selectorThree");
 const $option = document.querySelectorAll("select#selector > option");//Toma los option del selector
-const $ContainerDiv = document.querySelectorAll("div.container > p");
-
+const $ContainerDiv = document.querySelectorAll("div.card > p");
+const $ContainerCardTitle = document.querySelectorAll("div.card > h2");
 
 console.log($option);
 console.log($ContainerDiv);
-//DataBase
+// DataBase
 const optionList = [
     {
-        Marca: "Pitusas",
-        Modelo: "Con relleno"
+        Marca:"Pitusas",
+        Sabor: "Chocolate"
+    },
+    {
+        Marca:"Mana",
+        Sabor:"Vanilla"
+    }, 
+    {
+        Marca:"Chocolinas",
+        Sabor:"Chcolate"
+    },
+    {
+        Marca:"Don Satur"
     }
 ]
 const coloresList = {
@@ -19,10 +32,35 @@ const coloresList = {
     }
 }
 
-///
-$option.innerText = JSON.stringify(optionList[0]);
-$option[0].innerText = coloresList.Color.color;
-$option[1].innerText = "Marca";
-///
-$ContainerDiv[0].textContent = coloresList.Color.color;
-$ContainerDiv[1].textContent = coloresList.Color.color;
+// Function
+// $option.forEach((option) => { 
+//     option.textContent = optionList.filter((el)=> { el.Marca});
+// });
+
+for(let i=0;i < $option.length;i++){
+    $option[i].textContent = optionList[i].Marca;
+}
+for(let i=0;i<optionList.length;i++){
+    Newoption = document.createElement("option");
+    Newoption.textContent = optionList[i].Marca;
+    $SelectorTwo.appendChild(Newoption);
+}
+optionList.forEach(el => {
+    Newoption = document.createElement("option");
+    Newoption.textContent = el.Marca;
+    $SelectorThree.appendChild(Newoption);
+})
+
+// Events
+
+$Selector.addEventListener("change",(e) => {
+    $ContainerDiv[0].textContent = e.target.value;
+})
+
+$SelectorTwo.addEventListener("change",(e) => {
+    $ContainerDiv[1].textContent = e.target.value;
+})
+
+$SelectorThree.addEventListener("change",e =>{
+    $ContainerCardTitle[0].textContent = e.target.value;
+})
