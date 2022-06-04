2c3b6fbb6fb533dd4975aa5a3588e271cbf3526b
LucioFurnari <luciofurnari99@gmail.com>
2022-06-04 18:44:21 -0300

    Cambios en el style y agregado de imagenes

--- ResponsiveMenu/script.js
+++ ResponsiveMenu/script.js
@@ -1,6 +1,28 @@
 const Button = document.querySelector(".nav-button");
 const NavMenu = document.querySelector(".nav-links");
 
+const Img = document.querySelector(".img-gallery img")
+
+const Images = [
+    {
+        url:"images/image-1.jpg"
+    },
+    {
+        url:"images/image-2.jpg"
+    },
+    {
+        url:"images/image-3.jpg"
+    },
+    {
+        url:"images/image-4.jpg"
+    },
+    {
+        url:"images/image-5.jpg"
+    }
+];
+
+Img.setAttribute("src",Images[0].url)
+
 Button.addEventListener("click", () => {
     NavMenu.classList.toggle("nav-links-toggle")
 });
