4b0e749eb01686de9394e6767d5438e1c796a606
LucioFurnari <luciofurnari99@gmail.com>
2022-06-06 15:03:18 -0300

    Agregando mensaje de error e input vacio

--- Async/Api_Weather/js/app.js
+++ Async/Api_Weather/js/app.js
@@ -1,6 +1,12 @@
 const resultado = document.querySelector("#resultado");
 const formulario = document.querySelector("#formulario");
 
+const DivContainer = document.querySelector(".temp-container");
+const Temperatura = document.querySelector(".temp");
+const TemperaturaMaxima = document.querySelector(".temp-max");
+const TemperaturaMinima = document.querySelector(".temp-min");
+/////////
+
 window.addEventListener("load", () => {
     formulario.addEventListener("submit", buscarClima)
 })
@@ -11,7 +17,13 @@ function buscarClima(e) {
     const ciudad = document.querySelector("#ciudad").value
     const pais = document.querySelector("#pais").value
     if(ciudad == ""){
-        console.log("El input esta vacio");
+        const inputVacio = document.createElement("p");
+        inputVacio.textContent = "El input esta vacio"
+        formulario.appendChild(inputVacio);
+        setTimeout(() => {
+            formulario.removeChild(inputVacio)
+        }, 3000);
+        // console.log("El input esta vacio");
     }else {
         consultarAPI(ciudad,pais)
     }
@@ -24,9 +36,23 @@ function consultarAPI(ciudad, pais){
         .then(res => res.json())
         .then(data => {
             if(data.cod === "404"){
-                console.log("No existe la ciudad");
+                const MensajeError = document.createElement("p");
+                MensajeError.textContent = "No existe la ciudad"
+                MensajeError.style.display = "inline"
+                MensajeError.style.background = "red";
+                MensajeError.style.color = "white";
+                DivContainer.appendChild(MensajeError)
+                setTimeout(() => {
+                    DivContainer.removeChild(MensajeError);
+                }, 5000);
+                // console.log("No existe la ciudad");
+            }else {
+                Temperatura.textContent = `Temperatura: ${parseInt(data.main.temp - 273.15)} C°"`;
+                TemperaturaMaxima.textContent = `Temperatura Maxima: ${parseInt(data.main.temp_max - 273.15)} C°`;
+                TemperaturaMinima.textContent = `Temperatura Minima: ${parseInt(data.main.temp_min - 273.15)} C°`;
+                console.log(data)
+                console.log(data.main.temp);
             }
-            console.log(data)
         })
         
 }
