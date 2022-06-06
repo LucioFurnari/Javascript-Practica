770f71015eaa1ec8048cf2d6fe8f88c119d8eff2
LucioFurnari <luciofurnari99@gmail.com>
2022-06-06 19:12:33 -0300

    Agregando funcion callback y creacion de lista

--- Async/Api_Pollution/app.js
+++ Async/Api_Pollution/app.js
@@ -1,23 +1,35 @@
 const inputLatitude = document.querySelector("#latitude");
 const inputLongitude = document.querySelector("#longitude");
+const btnSubmit = document.querySelector("#submit");
+const form = document.querySelector("#formulario");
 
+const lista = document.querySelector(".list");
 
-
-
-function Apifetch(lat,lon) {
-    const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}
-                &lon=${lon}&appid=${"4bdda9b7b6a4d3a1dd20a4a879e28e91"}`    
-    const respuesta = fetch(url)
-    const data = respuesta.json()
-    return data
+function createList(key,value){
+    const list_item = document.createElement("li");
+    list_item.textContent = `${key}: ${value}`;
+    lista.appendChild(list_item)
+    setTimeout(() => {
+        lista.removeChild(list_item)
+    }, 5000);
 }
 
-
-
-async function test(){
+async function Apifetch(lat,lon){
     try {
-        await Apifetch(inputLatitude,inputLongitude);
-        console.log(Apifetch);
+        if(lat == "" || lon == "") {
+            console.log("No deje el formulario en blanco");
+        }else {
+            const latitude = parseInt(lat)
+            const longitude = parseInt(lon)
+            const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=${"4bdda9b7b6a4d3a1dd20a4a879e28e91"}`    
+            const respuesta = await fetch(url)
+            const data = await respuesta.json()
+            console.log(data);
+            const components = data.list[0].components
+            for (const property in components){
+                createList(property,components[property])
+            }
+        }
         // const respuesta = await fetch(url)
         // const data = await respuesta.json()
         // console.log(data);
@@ -27,12 +39,13 @@ async function test(){
         // for (const property in components){
         //     console.log(`${property}: ${components[property]}`);
         // }
-        // if(data.cod == "400"){
-        //     console.log("Error, no ingreso un numero");
-        // }
     } catch (err) {
         console.log(err);  
     }
 }
-test()
 
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    Apifetch(inputLatitude.value,inputLongitude.value)
+})
