7ebff67a923df77c642ba15c9c8438fa10f933e2
LucioFurnari <luciofurnari99@gmail.com>
2022-06-09 22:53:07 -0300

    Agregando style.css

--- Api_Weather/js/API_Call.js
+++ Api_Weather/js/API_Call.js
@@ -1,5 +1,8 @@
 
-const img = document.querySelector(".response-container img")
+const img = document.querySelector(".response-container img");
+const temp = document.querySelector(".response-container .temp");
+const tempMax = document.querySelector(".response-container .temp-max");
+const tempMin = document.querySelector(".response-container .temp-min");
 
 export async function APICall (city,country){
     const url =`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${"4bdda9b7b6a4d3a1dd20a4a879e28e91"}&units=metric `
@@ -7,4 +10,8 @@ export async function APICall (city,country){
     const data = await answer.json()
     console.log(data);
     img.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+
+    temp.textContent = `${data.main.temp} °C` ;
+    tempMax.textContent = data.main.temp_max;
+    tempMin.textContent = data.main.temp_min;
 }
