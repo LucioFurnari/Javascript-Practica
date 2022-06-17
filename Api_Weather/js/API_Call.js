5e635dea3ebc6b25bfa961dd9df79e5036c77d89
LucioFurnari <luciofurnari99@gmail.com>
2022-06-17 16:04:01 -0300

    Cambios en style.css

--- Api_Weather/js/API_Call.js
+++ Api_Weather/js/API_Call.js
@@ -5,13 +5,20 @@ const tempMax = document.querySelector(".response-container .temp-max");
 const tempMin = document.querySelector(".response-container .temp-min");
 
 export async function APICall (city,country){
-    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${"4bdda9b7b6a4d3a1dd20a4a879e28e91"}&units=metric `
-    const answer = await fetch(url)
-    const data = await answer.json()
-    console.log(data);
-    img.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-
-    temp.textContent = `${data.main.temp} °C` ;
-    tempMax.textContent = data.main.temp_max;
-    tempMin.textContent = data.main.temp_min;
+    const ApiKey ="4bdda9b7b6a4d3a1dd20a4a879e28e91";
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${ApiKey}&units=metric`
+    try{
+        const answer = await fetch(url)
+        const data = await answer.json()
+        console.log(data);
+        if(data.cod == 404){
+            temp.textContent = "Ciudad no encontrada";
+        }
+        img.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+        temp.textContent = `Temperatura: ${data.main.temp} °C` ;
+        tempMax.textContent = `Temp Max: ${data.main.temp_max} °C`;
+        tempMin.textContent = `Temp Min: ${data.main.temp_min} °C`;
+    }catch(error){
+        console.log(error);
+    }
 }
--- Api_Weather/js/app.js
+++ Api_Weather/js/app.js
@@ -1,13 +1,21 @@
 import { APICall } from "./API_Call.js";
 
-const form = document.querySelector("#weather-form")
+const form = document.querySelector("#weather-form"),
+    inputEmpty = document.createElement("p");
+
 
 
 form.addEventListener("submit", (event) => {
     const inputCity = document.querySelector("#city-name").value;
     const countrySelect = document.querySelector("#country-code").value;
-    event.preventDefault()
-    APICall(inputCity,countrySelect)
     console.log(inputCity);
     console.log(countrySelect);
+    event.preventDefault()
+    inputEmpty.textContent = "";
+    if(inputCity == ""){
+        form.appendChild(inputEmpty);
+        inputEmpty.textContent = "No deje el campo vacio";
+    }else {
+        APICall(inputCity,countrySelect)
+    }
 })
